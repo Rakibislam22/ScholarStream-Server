a6b2c92d1f9a04daa21cc1f120796c4db1977fda
Rakibislam22 <rakibislam13122018@gmail.com>
2025-12-14 16:36:49 +0600

    Config the Mongodb Connection

--- index.js
+++ index.js
@@ -17,7 +17,31 @@ app.get('/', (req, res) => {
 })
 
 
+const uri = process.env.URI;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 
