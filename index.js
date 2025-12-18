76afabe3b44a736ab7dd9d09389697793b199517
Rakibislam22 <rakibislam13122018@gmail.com>
2025-12-18 15:22:46 +0600

    Create Scholarship delete Api

--- index.js
+++ index.js
@@ -95,6 +95,15 @@ async function run() {
             }
         });
 
+        // Delete scholarship
+        app.delete('/scholarship/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const result = await scholarshipsCollection.deleteOne(query);
+            res.send(result);
+        });
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
